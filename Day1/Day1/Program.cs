899f52eedfa3c92017677850558d83ee77b09351
Lucas Ladreyt <l.ladreyt@avanade.com>
2021-12-01 14:01:57 +0100

    First day

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "values.txt";
+            if (!File.Exists(file))
+            {
+                Console.WriteLine("File doesn't exist");
+                return;
+            }
+
+            int increased = 0;
+            int index = 1;
+            var listValues = File.ReadLines(file).ToList();
+            foreach (string value in listValues.Skip(1))
+            {
+                if(int.TryParse(value, out int current) && int.TryParse(listValues[index - 1], out int previous))
+                    if (current > previous) increased++;
+                index++;
+            }
+
+            Console.Write($"Total increased {increased}");
+
+            return;
+        }
+    }
+}
