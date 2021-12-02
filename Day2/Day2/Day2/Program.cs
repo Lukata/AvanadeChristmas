86cbdf29af727d2ecf7908ca4673ae5ae0f29ac9
Lucas Ladreyt <l.ladreyt@avanade.com>
2021-12-02 09:05:19 +0100

    Step 2

--- Day2/Day2/Day2/Program.cs
+++ Day2/Day2/Day2/Program.cs
@@ -15,7 +15,48 @@ namespace Day2
                 Console.WriteLine("File doesn't exist");
                 return;
             }
+            //First step
+            FirstStep(file);
 
+            //second step
+            SecondStep(file);
+        }
+
+        private static void SecondStep(string file)
+        {
+            int x = 0;
+            int y = 0;
+            int aim = 0;
+            var values = File.ReadLines(file).ToList();
+
+            foreach (string value in values)
+            {
+                var splitted = value.Split(' ');
+                if (int.TryParse(splitted[1], out int command))
+                {
+                    switch (splitted[0].ToLower())
+                    {
+                        case "up":
+                            aim -= command;
+                            break;
+                        case "down":
+                            aim += command;
+                            break;
+                        case "forward":
+                            x += command;
+                            if(aim != 0)
+                                y += aim * command;
+                            break;
+                    }
+                }
+            }
+
+            int r = x * y;
+            Console.WriteLine($"horrizontal: {x} vertical: {y} total: {r}");
+        }
+
+        private static void FirstStep(string file)
+        {
             int x = 0;
             int y = 0;
             var values = File.ReadLines(file).ToList();
