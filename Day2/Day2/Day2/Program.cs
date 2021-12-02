cba916c077991835b616915ee0e97b38c68f4b59
Lucas Ladreyt <l.ladreyt@avanade.com>
2021-12-02 08:52:43 +0100

    first step

---
+++ Day2/Day2/Day2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "input.txt";
+
+            if (!File.Exists(file))
+            {
+                Console.WriteLine("File doesn't exist");
+                return;
+            }
+
+            int x = 0;
+            int y = 0;
+            var values = File.ReadLines(file).ToList();
+            foreach (string value in values)
+            {
+                var splitted = value.Split(' ');
+                if (int.TryParse(splitted[1], out int command))
+                {
+                    switch (splitted[0].ToLower())
+                    {
+                        case "up":
+                            y -= command;
+                            break;
+                        case "down":
+                            y += command;
+                            break;
+                        case "forward":
+                            x += command;
+                            break;
+                    }
+                }
+            }
+            int r = x * y;
+            Console.WriteLine($"horrizontal: {x} vertical: {y} total: {r}");
+        }
+    }
+}
