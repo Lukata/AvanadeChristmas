994857fcf042d3d7860052f168b0a78bd344a219
F.Goret-Quenet <f.goret-quenet@avanade.com>
2021-12-02 18:13:08 +0100

    day 1

---
+++ AdventOfCode/AdventOfCode.Day1/InputReader.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace AdventOfCode.Day1
+{
+    public static class InputReader
+    {
+        public static int[] ReadFile()
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            int[] inputs = new int[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                inputs[i] = int.Parse(lines[i]);
+            }
+
+            return inputs;
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Day1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AdventOfCode.Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputs = InputReader.ReadFile();
+
+            PartTwo(inputs);
+        }
+
+        private static void PartOne(int[] inputs)
+        {
+            int count = 0;
+
+            for (int i = 1; i < inputs.Length; i++)
+            {
+                if (inputs[i - 1] < inputs[i])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static void PartTwo(int[] inputs)
+        {
+            int count = 0;
+
+            for (int i = 1; i < inputs.Length - 2; i++)
+            {
+                int commomPart = inputs[i] + inputs[i + 1];
+                int firtPart = inputs[i - 1] + commomPart;
+                int secondPart = inputs[i + 2] + commomPart;
+
+                if (firtPart < secondPart)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
